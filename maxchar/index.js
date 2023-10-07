22186c7baab1e06f40b14cb57a34e4c5c520af07
accel33 <accelwtf@gmail.com>
2023-10-06 20:36:46 -0500

    Add: Comments

--- maxchar/index.js
+++ maxchar/index.js
@@ -10,20 +10,21 @@ function maxChar(str) {
     let maxNum = 0
     let maxChar = ''
     // Recorrer el String como Array y ponerlo en un Objeto Map
-    for (const char of str) {
-        if (!charMap[char]) {
-            charMap[char] = 1
+    for (const char of str) { // 'a','b','c','c','c'...
+        if (!charMap[char]) { // charMap['a'] = undefined
+            charMap[char] = 1 // charMap['a'] = 1
         } else {
-            charMap[char]++
+            charMap[char]++ // charMap['a'] = 1+1 = 2
         }
     }
     // Recorrer el Objeto Map como Objeto y asignar numero maximo con caracter maximo
-    for (const char in charMap) {
+    for (const char in charMap) { // {'a':1,'b':1,'c':3}
         if (charMap[char] > maxNum) {
             maxNum = charMap[char]
             maxChar = char
         }
     }
+    console.log(charMap);
     return maxChar
 }
 
